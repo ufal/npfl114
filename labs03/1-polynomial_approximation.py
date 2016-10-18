e093274e7848da9f8d9418ab4fb931c88a50af7a
Milan Straka <milan@strakovi.com>
2016-10-18 03:00:22 +0200

    Update the polynomial_approximation example.

--- labs03/1-polynomial_approximation.py
+++ labs03/1-polynomial_approximation.py
@@ -5,20 +5,23 @@ import datetime
 import numpy as np
 import tensorflow as tf
 
+import math
+
 import matplotlib.pyplot as plt
 
 class Network:
-    def __init__(self, model_degree, learning_rate, threads):
+    def __init__(self, layers, layer_size, threads):
         # Construct the graph
         self.xs = tf.placeholder(tf.float32, [None])
         self.ys = tf.placeholder(tf.float32, [None])
 
-        self.predictions = tf.zeros_like(self.xs)
-        for i in range(0,model_degree+1):
-            self.predictions += tf.pow(self.xs, i) * tf.Variable(tf.random_normal([]))
+        hidden = tf.contrib.layers.fully_connected(tf.reshape(self.xs, [-1,1]), num_outputs=layer_size, activation_fn=tf.nn.relu)
+        for i in range(1,layers):
+            hidden = tf.contrib.layers.fully_connected(hidden, num_outputs=layer_size, activation_fn=tf.nn.relu)
+        self.predictions = tf.contrib.layers.fully_connected(hidden, num_outputs=1, activation_fn=None)
 
         loss = tf.reduce_mean(tf.pow(self.predictions - self.ys, 2))
-        self.training = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+        self.training = tf.train.AdamOptimizer().minimize(loss)
 
         # Create the session
         self.session = tf.Session(config=tf.ConfigProto(inter_op_parallelism_threads=args.threads,
@@ -40,31 +43,31 @@ if __name__ == '__main__':
     # Parse arguments
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_degree', default=5, type=int, help='Polynomial degree of data.')
-    parser.add_argument('--data_points', default=100, type=int, help='Data points.');
-    parser.add_argument('--model_degree', default=5, type=int, help='Polynomial degree of model.')
-    parser.add_argument('--learning_rate', default=0.01, type=int, help='Learning rate.')
+    parser.add_argument('--degree', default=5, type=int, help='Polynomial degree of data.')
+    parser.add_argument('--points', default=100, type=int, help='Data points.');
+    parser.add_argument('--layers', default=2, type=int, help='Data points.');
+    parser.add_argument('--layer_size', default=30, type=int, help='Data points.');
     parser.add_argument('--batch_size', default=1, type=int, help='Batch size.')
-    parser.add_argument('--epochs', default=100, type=int, help='Number of epochs.')
+    parser.add_argument('--epochs', default=400, type=int, help='Number of epochs.')
     parser.add_argument('--threads', default=1, type=int, help='Maximum number of threads to use.')
     args = parser.parse_args()
 
-    xs = np.linspace(0, 1, args.data_points)
+    xs = np.linspace(0, 1, args.points)
 
     ys = np.ones_like(xs)
-    for i in range(args.data_degree):
-        ys *= np.e * (xs - i / (args.data_degree - 1))
-    ys += np.random.uniform(-0.1, 0.1, args.data_points)
+    for i in range(args.degree):
+        ys *= 3 * (xs - i / (args.degree - 1))
+    ys += np.random.uniform(-0.1, 0.1, args.points)
 
     plt.ion()
     plt.scatter(xs, ys)
     plt.draw()
 
     # Construct the network
-    network = Network(model_degree=args.model_degree, learning_rate=args.learning_rate, threads=args.threads)
+    network = Network(layers=args.layers, layer_size=args.layer_size, threads=args.threads)
 
     for i in range(args.epochs):
-        permutation = np.random.permutation(args.data_points)
+        permutation = np.random.permutation(args.points)
         while permutation.size:
             batch_indices = permutation[:args.batch_size]
             permutation = permutation[args.batch_size:]
