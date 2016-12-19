a05153e63e06e16da219c841c3c64f5653d86ba5
Milan Straka <milan@strakovi.com>
2016-12-19 12:26:47 +0100

    Add reinforce_with_baseline_pixels-skeleton.

---
+++ labs11/environment_pixels.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+from __future__ import division
+from __future__ import print_function
+
+import gym
+import gym.envs.classic_control.rendering as gym_rendering
+import numpy as np
+
+class EnvironmentPixels:
+    def __init__(self, env_name):
+        self._env_name = env_name
+        self._env = gym.make(env_name)
+        self._iw = gym_rendering.SimpleImageViewer()
+        if self._env_name.startswith("CartPole"):
+            self._images = 3
+            self._image = np.zeros([80, 80, self._images], dtype=np.float32)
+        else:
+            raise ValueError("Environment {} not supported in EnvironmentPixels".format(env_name))
+
+    def _draw(self, observation):
+        if self._env_name.startswith("CartPole"):
+            for i in range(self._images - 1):
+                self._image[:, :, i] = self._image[:, :, i + 1]
+            cart = 40 + observation[0] / 3 * 40
+            pole_x = int(40 + (observation[0] + np.sin(observation[2]) * 4.2) / 3 * 40)
+            pole_y = int(70 - np.cos(observation[2]) * 5.2 / 3 * 40)
+            self._image[:, :, self._images - 1] = 0
+            self._fill_polygon([(70, cart-10), (80, cart-10), (80, cart+10), (70, cart+10)], self._image[:, :, self._images - 1], 0.5)
+            self._fill_polygon([(pole_y, pole_x-2), (70, cart-2), (70, cart+2), (pole_y, pole_x+2)], self._image[:, :, self._images - 1], 1)
+            return self._image
+
+    @property
+    def observations(self):
+        return list(self._image.shape)
+
+    @property
+    def actions(self):
+        return self._env.action_space.n
+
+    def reset(self):
+        observation = self._env.reset()
+        for step in range(self._images - 1):
+            self._draw(observation)
+        return self._draw(observation)
+
+    def step(self,action):
+        observation, reward, done, info = self._env.step(action)
+
+        return self._draw(observation), reward, done, info
+
+    def render(self):
+        if self._env_name.startswith("CartPole"):
+            self._iw.imshow((self._image[:, :, :3]*255).astype(np.uint8))
+
+    # Taken from https://github.com/luispedro/mahotas/blob/master/mahotas/polygon.py
+    def _fill_polygon(self, polygon, canvas, color=1):
+        '''
+        fill_polygon([(y0,x0), (y1,x1),...], canvas, color=1)
+        Draw a filled polygon in canvas
+        Parameters
+        ----------
+        polygon : list of pairs
+            a list of (y,x) points
+        canvas : ndarray
+            where to draw, will be modified in place
+        color : integer, optional
+            which colour to use (default: 1)
+        '''
+        # algorithm adapted from: http://www.alienryderflex.com/polygon_fill/
+        if not len(polygon):
+            return
+        min_y = min(y for y,x in polygon)
+        max_y = max(y for y,x in polygon)
+        polygon = [(float(y),float(x)) for y,x in polygon]
+        if max_y < canvas.shape[0]:
+            max_y += 1
+        for y in range(min_y, max_y):
+            nodes = []
+            j = -1
+            for i,p in enumerate(polygon):
+                pj = polygon[j]
+                if p[0] < y and pj[0] >= y or pj[0] < y and p[0] >= y:
+                    dy = pj[0] - p[0]
+                    if dy:
+                        nodes.append( (p[1] + (y-p[0])/(pj[0]-p[0])*(pj[1]-p[1])) )
+                    elif p[0] == y:
+                        nodes.append(p[1])
+                j = i
+            nodes.sort()
+            for n,nn in zip(nodes[::2],nodes[1::2]):
+                nn += 1
+                canvas[y, int(n):int(nn)] = color
+
---
+++ labs11/reinforce_with_baseline_pixels-skeleton.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+from __future__ import division
+from __future__ import print_function
+
+import environment_pixels
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.layers as tf_layers
+
+class PolicyGradientWithBaseline:
+    def __init__(self, observation_shape, policy_and_value_network, learning_rate, threads=1, seed=42):
+        # Create an empty graph and a session
+        graph = tf.Graph()
+        graph.seed = seed
+        self.session = tf.Session(graph = graph, config=tf.ConfigProto(inter_op_parallelism_threads=threads,
+                                                                       intra_op_parallelism_threads=threads))
+
+        # Construct the graph
+        with self.session.graph.as_default():
+            self.observations = tf.placeholder(tf.float32, [None] + observation_shape)
+            # TODO: define the following, using policy_and_value_network
+            # logits = ...
+            # self.value = ...
+            # self.probabilities = ... [probabilities of all actions]
+
+            self.chosen_actions = tf.placeholder(tf.int32, [None])
+            self.returns = tf.placeholder(tf.float32, [None])
+
+            # TODO: compute loss of both the policy and value
+            # loss_policy = ... [cross_entropy between logits and chosen_actions,
+            #                    multiplied by (self.returns - tf.stop_gradient(self.value))]
+            # loss_value = ... [MSE of self.return and self.value]
+            # self.training = ... [use loss_policy + loss_value, and use learning_rate]
+
+            # Initialize variables
+            self.session.run(tf.initialize_all_variables())
+
+    def predict(self, observations):
+        return self.session.run(self.probabilities,
+                                {self.observations: observations})
+
+    def train(self, observations, chosen_actions, returns):
+        self.session.run(self.training,
+                         {self.observations: observations,
+                          self.chosen_actions: chosen_actions,
+                          self.returns: returns})
+
+if __name__ == "__main__":
+    # Fix random seed
+    np.random.seed(42)
+
+    # Parse arguments
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", default="CartPole-v1", type=str, help="Name of the environment.")
+    parser.add_argument("--episodes", default=100000, type=int, help="Episodes in a batch.")
+    parser.add_argument("--max_steps", default=500, type=int, help="Maximum number of steps.")
+    parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
+    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+
+    parser.add_argument("--alpha", default=0.001, type=float, help="Learning rate.")
+    parser.add_argument("--gamma", default=1.0, type=float, help="Discounting factor.")
+    parser.add_argument("--batch_size", default=1, type=int, help="Number of episodes to train on.")
+    args = parser.parse_args()
+
+    # Create the environment
+    env = environment_pixels.EnvironmentPixels(args.env)
+    if args.render_each:
+        # Because of low TLS limit, load OpenGL before TensorFlow
+        env.reset()
+        env.render()
+
+    # Create policy and value network
+    def policy_and_value_network(observations):
+        # TODO: Baseline network, used in (Mnih et al., 2016)
+        conv = tf_layers.convolution2d(observations, 16, 8, 4)
+        conv = tf_layers.convolution2d(conv, 32, 4, 2)
+        conv = tf_layers.flatten(conv)
+        hidden_layer = tf_layers.fully_connected(conv, 128)
+        logits = tf_layers.linear(hidden_layer, env.actions)
+        value = tf_layers.linear(hidden_layer, 1)
+        # TODO: If you do not want to use baseline, uncomment the next line
+#         value = tf.zeros([tf.shape(observations)[0], 1])
+        return logits, value
+
+    pg = PolicyGradientWithBaseline(observation_shape=env.observations, policy_and_value_network=policy_and_value_network,
+                                    learning_rate=args.alpha, threads=args.threads)
+
+    episode_returns, episode_lengths = [], []
+    for batch_start in range(0, args.episodes, args.batch_size):
+        # Collect data for training
+        observations, actions, rewards = [], [], []
+        for episode in range(batch_start, batch_start + args.batch_size):
+            # Perform episode
+            observation = env.reset()
+            total_reward = 0
+            for t in range(args.max_steps):
+                if args.render_each and episode > 0 and episode % args.render_each == 0:
+                    env.render()
+
+                # TODO: compute probabilities and value function using pg.predict, and choose action according to the probabilities
+                # probabilities = ...
+                # action = ...
+
+                observations.append(observation)
+                actions.append(action)
+
+                # perform step in the environment
+                observation, reward, done, _ = env.step(action)
+
+                total_reward += reward
+                rewards.append(reward)
+
+                if done:
+                    break
+
+            # TODO: sum and discount rewards, only the last t of them
+
+            episode_returns.append(total_reward)
+            episode_lengths.append(t)
+            if len(episode_returns) % 10 == 0:
+                # Evaluate
+                observation, total_reward = env.reset(), 0
+                for i in range(args.max_steps):
+                    [probabilities] = pg.predict([observation])
+                    action = np.random.choice(np.arange(len(probabilities)), p=probabilities)
+                    observation, reward, done, _ = env.step(action)
+                    total_reward += reward
+                    if done:
+                        break
+
+                print("Episode {}, current evaluation reward {}, mean 100-episode reward {}, mean 100-episode length {}.".format(
+                    episode + 1, total_reward, np.mean(episode_returns[-100:]), np.mean(episode_lengths[-100:])))
+
+        pg.train(observations, actions, rewards)
