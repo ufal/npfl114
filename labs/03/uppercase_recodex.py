576cf61164091c3e6a8c24de06fb3ff9e7daa658
Milan Straka <milan@strakovi.com>
2018-03-18 15:21:11 +0100

    Compress the test set data.
    Also specify the source encoding as UTF-8, which is assumed to be used
    by the sources.

--- labs/03/uppercase_recodex.py
+++ labs/03/uppercase_recodex.py
@@ -19,12 +19,32 @@ def as_triple_quoted_literal(s):
 
     return "".join(res)
 
+def as_compressed_ascii_encoded(s):
+    import base64
+    import io
+    import lzma
+
+    with io.BytesIO() as lzma_data:
+        with lzma.open(lzma_data, mode="wt", encoding="utf-8") as lzma_file:
+            lzma_file.write(s)
+        return base64.b85encode(lzma_data.getbuffer())
+
 with open("uppercase_recodex_submission.py", "w", encoding="utf-8") as submission:
+    print("# coding=utf-8", file=submission, end="\n\n")
+
     for i, path in enumerate(args.sources):
         with open(path, "r", encoding="utf-8") as source:
             print("source_{} =".format(i + 1), as_triple_quoted_literal(source.read()), file=submission, end="\n\n")
 
     with open(args.test_data, "r", encoding="utf-8") as test:
-        print("test_data =", as_triple_quoted_literal(test.read()), file=submission, end="\n\n")
+        print("test_data =", as_compressed_ascii_encoded(test.read()), file=submission, end="\n\n")
 
-    print("print(test_data, end=\"\")", file=submission)
+    print("""if __name__ == "__main__":
+    import base64
+    import io
+    import lzma
+    import sys
+
+    with io.BytesIO(base64.b85decode(test_data)) as lzma_data:
+        with lzma.open(lzma_data, "r") as lzma_file:
+            sys.stdout.buffer.write(lzma_file.read())""", file=submission)
