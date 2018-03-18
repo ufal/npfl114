44e68c43dd0bc16858211002baae2ac3e5a8efd5
Milan Straka <milan@strakovi.com>
2018-03-18 21:42:40 +0100

    Add mnist_competition task.

---
+++ labs/04/mnist_competition.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+import numpy as np
+import tensorflow as tf
+
+class Network:
+    WIDTH = 28
+    HEIGHT = 28
+    LABELS = 10
+
+    def __init__(self, threads, seed=42):
+        # Create an empty graph and a session
+        graph = tf.Graph()
+        graph.seed = seed
+        self.session = tf.Session(graph = graph, config=tf.ConfigProto(inter_op_parallelism_threads=threads,
+                                                                       intra_op_parallelism_threads=threads))
+
+    def construct(self, args):
+        with self.session.graph.as_default():
+            # TODO: Construct the network and training operation.
+
+            # Summaries
+            self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.labels, self.predictions), tf.float32))
+            summary_writer = tf.contrib.summary.create_file_writer(args.logdir, flush_millis=10 * 1000)
+            self.summaries = {}
+            with summary_writer.as_default(), tf.contrib.summary.record_summaries_every_n_global_steps(100):
+                self.summaries["train"] = [tf.contrib.summary.scalar("train/loss", loss),
+                                           tf.contrib.summary.scalar("train/accuracy", self.accuracy)]
+            with summary_writer.as_default(), tf.contrib.summary.always_record_summaries():
+                for dataset in ["dev", "test"]:
+                    self.summaries[dataset] = [tf.contrib.summary.scalar(dataset + "/loss", loss),
+                                               tf.contrib.summary.scalar(dataset + "/accuracy", self.accuracy)]
+
+            # Initialize variables
+            self.session.run(tf.global_variables_initializer())
+            with summary_writer.as_default():
+                tf.contrib.summary.initialize(session=self.session, graph=self.session.graph)
+
+    def train(self, images, labels):
+        # TODO
+        pass
+
+    def evaluate(self, dataset, images, labels):
+        # TODO
+        pass
+
+
+if __name__ == "__main__":
+    import argparse
+    import datetime
+    import os
+    import re
+
+    # Fix random seed
+    np.random.seed(42)
+
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
+    parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
+    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    args = parser.parse_args()
+
+    # Create logdir name
+    args.logdir = "logs/{}-{}-{}".format(
+        os.path.basename(__file__),
+        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+    )
+    if not os.path.exists("logs"): os.mkdir("logs") # TF 1.6 will do this by itself
+
+    # Load the data
+    from tensorflow.examples.tutorials import mnist
+    mnist = mnist.input_data.read_data_sets("mnist-gan", reshape=False, seed=42)
+
+    # Construct the network
+    network = Network(threads=args.threads)
+    network.construct(args)
+
+    # Train
+    for i in range(args.epochs):
+        while mnist.train.epochs_completed == i:
+            images, labels = mnist.train.next_batch(args.batch_size)
+            network.train(images, labels)
+
+        network.evaluate("dev", mnist.validation.images, mnist.validation.labels)
+
+    # TODO: Compute test_labels, as numbers 0-9, corresponding to mnist.test.images
+
+    for label in test_labels:
+        print(label)
---
+++ labs/04/mnist_competition_recodex.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("test_data", type=str, help="Path to test data.")
+parser.add_argument("sources", type=str, nargs="+", help="Path to sources.")
+args = parser.parse_args()
+
+def as_triple_quoted_literal(s):
+    res = ['"'] * 3
+    for char in s:
+        if char == '"':
+            res.append('\\"')
+        elif char == "\\":
+            res.append("\\\\")
+        else:
+            res.append(char)
+    res += ['"'] * 3
+
+    return "".join(res)
+
+def as_compressed_ascii_encoded(s):
+    import base64
+    import io
+    import lzma
+
+    with io.BytesIO() as lzma_data:
+        with lzma.open(lzma_data, mode="wt", encoding="utf-8") as lzma_file:
+            lzma_file.write(s)
+        return base64.b85encode(lzma_data.getbuffer())
+
+with open("mnist_competition_recodex_submission.py", "w", encoding="utf-8") as submission:
+    print("# coding=utf-8", file=submission, end="\n\n")
+
+    for i, path in enumerate(args.sources):
+        with open(path, "r", encoding="utf-8") as source:
+            print("source_{} =".format(i + 1), as_triple_quoted_literal(source.read()), file=submission, end="\n\n")
+
+    with open(args.test_data, "r", encoding="utf-8") as test:
+        print("test_data =", as_compressed_ascii_encoded(test.read()), file=submission, end="\n\n")
+
+    print("""if __name__ == "__main__":
+    import base64
+    import io
+    import lzma
+    import sys
+
+    with io.BytesIO(base64.b85decode(test_data)) as lzma_data:
+        with lzma.open(lzma_data, "r") as lzma_file:
+            sys.stdout.buffer.write(lzma_file.read())""", file=submission)
