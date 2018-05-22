54eadeaf245cb7c90944f35387172a580b4b4cf3
Milan Straka <milan@strakovi.com>
2018-05-22 12:08:35 +0200

    Clarify that the whole dict returned by `evaluate` should be printed.

--- labs/13/estimator_mnist.py
+++ labs/13/estimator_mnist.py
@@ -83,8 +83,8 @@ if __name__ == "__main__":
 
         # TODO: Define validation input_fn similarly, but without suffling.
         # TODO: Evaluate the validation data, using `model.evaluate` with `name="dev"` option
-        # and print its return value.
+        # and print its return value (which is a dictionary with accuracy, loss and global_step).
 
     # TODO: Define input_fn for test set as for validation data.
     # TODO: Evaluate the test set using `model.evaluate` with `name="test"` option
-    # and print its return value.
+    # and print its return value (which is a dictionary with accuracy, loss and global_step).
