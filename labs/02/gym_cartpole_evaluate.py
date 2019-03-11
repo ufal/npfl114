7490fc212985f2ecfab7f4c06c68b4620f9cb6f0
Milan Straka <milan@strakovi.com>
2019-03-11 09:58:36 +0100

    Add gym_cartpole assignment.

---
+++ labs/02/gym_cartpole_evaluate.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import argparse
+import datetime
+import os
+import re
+
+import gym
+import numpy as np
+import tensorflow as tf
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("model", default="gym_cartpole_model.h5", nargs="?", type=str, help="Name of model.")
+parser.add_argument("--episodes", default=100, type=int, help="Number of episodes.")
+parser.add_argument("--render", default=False, action="store_true", help="Render the environment.")
+parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+args = parser.parse_args()
+
+# Use given number of threads
+tf.config.threading.set_inter_op_parallelism_threads(args.threads)
+tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+
+# Load the model
+model = tf.keras.models.load_model(args.model, compile=False)
+
+# Create the environment
+env = gym.make("CartPole-v1")
+env.seed(42)
+
+# Evaluate the episodes
+total_score = 0
+for episode in range(args.episodes):
+    observation = env.reset()
+    score = 0
+    for i in range(env.spec.timestep_limit):
+        if args.render:
+            env.render()
+        observation, reward, done, info = env.step(np.argmax(model.predict(observation[np.newaxis, ...])[0]))
+        score += reward
+        if done:
+            break
+
+    total_score += score
+    print("The episode {} finished with score {}.".format(episode + 1, score))
+
+print("The average reward per episode was {:.2f}.".format(total_score / args.episodes))
---
+++ tasks/gym_cartpole.md
@@ -0,0 +1,26 @@
+### Assignment: gym_cartpole
+#### Date: Deadline: Mar 24, 23:59
+#### Points: 4 points
+
+Solve the [CartPole-v1 environment](https://gym.openai.com/envs/CartPole-v1)
+from the [OpenAI Gym](https://gym.openai.com/), utilizing only provided supervised
+training data. The data is available in
+[gym_cartpole-data.txt](https://github.com/ufal/npfl114/tree/master/labs/02/gym_cartpole-data.txt)
+file, each line containing one observation (four space separated floats) and
+a corresponding action (the last space separated integer). Start with the
+[gym_cartpole.py](https://github.com/ufal/npfl114/tree/master/labs/02/gym_cartpole.py).
+
+The solution to this task should be a _model_ which passes evaluation on random
+inputs. This evaluation is performed by running the
+[gym_cartpole_evaluate.py](https://github.com/ufal/npfl114/tree/master/labs/02/gym_cartpole_evaluate.py),
+which loads a model and then evaluates it on 100 random episodes (optionally
+rendering if `--render` option is provided). In order to pass, you must achieve
+an average reward of at least 475 on 100 episodes.
+
+_The size of the training data is very small and you should consider
+it when designing the model._
+
+When submitting your model to ReCodEx, submit:
+- one file with the model itself (with `h5` suffix),
+- the source code (or multiple sources) used to train the model (with `py` suffix),
+  and possibly indicating teams.
