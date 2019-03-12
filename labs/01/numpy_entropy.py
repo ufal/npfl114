12a14e571b6f9f33bee499387bc50b1d07780408
Ondrej Lukas <ondrej.lukas95@gmail.com>
2019-03-12 16:38:30 +0100

    Value error solved

--- labs/01/numpy_entropy.py
+++ labs/01/numpy_entropy.py
@@ -18,26 +18,36 @@ if __name__ == "__main__":
             else:
                 data_counts[line] += 1
 
-    probs = []
+    data_probs = {} 
     for key in data_counts.keys():
         count = data_counts[key]
-        probs.append(count / seq_len)
-    data_dist = np.array(probs)
+        data_probs[key.lower()] = count / seq_len
 
 
     # Load model distribution, each line `word \t probability`.
-    model_probs = []
+    model_probs = {}
     with open("numpy_entropy_model.txt", "r") as model:
         for line in model:
             line = line.rstrip("\n")
             split = line.split("\t")
-            model_probs.append(float(split[1]))
-    model_dist = np.array(model_probs)
+            model_probs[split[0].lower()] = (np.float(split[1]))
+    
+    for k in data_probs:
+        if k not in model_probs.keys():
+            model_probs[k] = 0
+    for k in model_probs:
+        if k not in data_probs.keys():
+            data_probs[k] = 0
+    print(data_probs)
+    print(model_probs)
+
+    data_dist = np.array([data_probs[k] for k in sorted(data_probs.keys())])
+    model_dist = np.array([model_probs[k] for k in sorted(model_probs.keys())])
 
     # Data entropy
-    entropy = -np.sum(data_dist * np.log(data_dist))
+    tmp = data_dist[np.nonzero(data_dist)]
+    entropy = -np.sum(tmp * np.log(tmp))
     print("{:.2f}".format(entropy))
-
     #Data and model cross-entropy
     cross_entropy = -np.sum(data_dist * np.log(model_dist))
     print("{:.2f}".format(cross_entropy))
