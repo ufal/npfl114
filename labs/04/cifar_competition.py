fb0ab6d33ae33aaba82aaeb0672ab98dfd69cd6b
Milan Straka <milan@strakovi.com>
2019-03-25 17:54:59 +0100

    Pass correct number of epochs.

--- labs/04/cifar_competition.py
+++ labs/04/cifar_competition.py
@@ -22,7 +22,7 @@ class Network(tf.keras.Model):
     def train(self, cifar, args):
         self.fit(
             cifar.train.data["images"], cifar.train.data["labels"],
-            batch_size=args.batch_size, epochs=1,
+            batch_size=args.batch_size, epochs=args.epochs,
             validation_data=(cifar.dev.data["images"], cifar.dev.data["labels"]),
             callbacks=[self.tb_callback],
         )
