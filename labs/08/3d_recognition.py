02db7e5430da87431f6c70b99c731f6609704061
Milan Straka <milan@strakovi.com>
2019-04-22 22:53:01 +0200

    Add 3d_recognition assignment and virtual slides for Easter Monday.

---
+++ labs/08/3d_recognition.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import numpy as np
+import tensorflow as tf
+
+from modelnet import ModelNet
+
+# The neural network model
+class Network:
+    def __init__(self, modelnet, args):
+        # TODO: Define a suitable model, and either `.compile` it, or prepare
+        # optimizer and loss manually.
+
+        self.tb_callback=tf.keras.callbacks.TensorBoard(args.logdir, update_freq=1000, profile_batch=1)
+        self.tb_callback.on_train_end = lambda *_: None
+
+    def train(self, modelnet, args):
+        # TODO: Train the network on a given dataset.
+        raise NotImplementedError()
+
+    def predict(self, dataset, args):
+        # TODO: Predict method should return a list/np.ndarray of
+        # label probabilities from the test set
+        raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    import argparse
+    import datetime
+    import os
+    import re
+
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
+    parser.add_argument("--modelnet", default=20, type=int, help="ModelNet dimension.")
+    parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
+    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    args = parser.parse_args()
+
+    # Fix random seeds
+    np.random.seed(42)
+    tf.random.set_seed(42)
+    tf.config.threading.set_inter_op_parallelism_threads(args.threads)
+    tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+
+    # Create logdir name
+    args.logdir = os.path.join("logs", "{}-{}-{}".format(
+        os.path.basename(__file__),
+        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+    ))
+
+    # Load data
+    modelnet = ModelNet(args.modelnet)
+
+    # Create the network and train
+    network = Network(modelnet, args)
+    network.train(modelnet, args)
+
+    # Generate test set annotations, but in args.logdir to allow parallel execution.
+    out_path = "3d_recognition_test.txt"
+    if os.path.isdir(args.logdir): out_path = os.path.join(args.logdir, out_path)
+    with open(out_path, "w", encoding="utf-8") as out_file:
+        for probs in network.predict(modelnet.test, args):
+            print(np.argmax(probs), file=out_file)
---
+++ lectures/lecture08.md
@@ -0,0 +1,5 @@
+### Lecture: 8. Easter Monday
+#### Date: Apr 22
+#### Lecture assignment: 3d_recognition
+
+_Easter Monday_
