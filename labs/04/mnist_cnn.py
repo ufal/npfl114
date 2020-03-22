6d3b108f42f62732d8a35441b1baa0a5ab521db9
Milan Straka <milan@strakovi.com>
2020-03-22 16:56:41 +0100

    Add mnist_cnn assignment.

---
+++ labs/04/mnist_cnn.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+import argparse
+import datetime
+import os
+import re
+
+import numpy as np
+import tensorflow as tf
+
+from mnist import MNIST
+
+# The neural network model
+class Network:
+    def __init__(self, args):
+        # TODO: Create the model. The template uses functional API, but
+        # feel free to use subclassing if you want.
+        inputs = tf.keras.layers.Input(shape=[MNIST.H, MNIST.W, MNIST.C])
+
+        # TODO: Add CNN layers specified by `args.cnn`, which contains
+        # comma-separated list of the following layers:
+        # - `C-filters-kernel_size-stride-padding`: Add a convolutional layer with ReLU
+        #   activation and specified number of filters, kernel size, stride and padding.
+        # - `CB-filters-kernel_size-stride-padding`: Same as `C`, but use batch normalization.
+        #   In detail, start with a convolutional layer without bias and activation,
+        #   then add batch normalization layer, and finally ReLU activation.
+        # - `M-kernel_size-stride`: Add max pooling with specified size and stride.
+        # - `R-[layers]`: Add a residual connection. The `layers` contain a specification
+        #   of at least one convolutional layer (but not a recursive residual connection `R`).
+        #   The input to the specified layers is then added to their output.
+        # - `F`: Flatten inputs. Must appear exactly once in the architecture.
+        # - `H-hidden_layer_size`: Add a dense layer with ReLU activation and specified size.
+        # - `D-dropout_rate`: Apply dropout with the given dropout rate.
+        # You can assume the resulting network is valid; it is fine to crash if it is not.
+        #
+        # Produce the results in variable `hidden`.
+
+        # Add the final output layer
+        outputs = tf.keras.layers.Dense(MNIST.LABELS, activation=tf.nn.softmax)(hidden)
+
+        self._model = tf.keras.Model(inputs=inputs, outputs=outputs)
+        self._model.compile(
+            optimizer=tf.keras.optimizers.Adam(),
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+            metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
+        )
+        self._tb_callback=tf.keras.callbacks.TensorBoard(args.logdir, histogram_freq=1, update_freq=100, profile_batch=0)
+
+    def train(self, mnist, args):
+        self._model.fit(
+            mnist.train.data["images"], mnist.train.data["labels"],
+            batch_size=args.batch_size, epochs=args.epochs,
+            validation_data=(mnist.dev.data["images"], mnist.dev.data["labels"]),
+            callbacks=[self._tb_callback],
+        )
+
+    def test(self, mnist, args):
+        test_logs = self._model.evaluate(mnist.test.data["images"], mnist.test.data["labels"], batch_size=args.batch_size)
+        self._tb_callback.on_epoch_end(1, {"val_test_" + metric: value for metric, value in zip(self._model.metrics_names, test_logs)})
+        return test_logs[self._model.metrics_names.index("accuracy")]
+
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
+    parser.add_argument("--cnn", default=None, type=str, help="CNN architecture.")
+    parser.add_argument("--epochs", default=30, type=int, help="Number of epochs.")
+    parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
+    parser.add_argument("--seed", default=42, type=int, help="Random seed.")
+    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    parser.add_argument("--verbose", default=False, action="store_true", help="Verbose TF logging.")
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+
+    # Fix random seeds and threads
+    np.random.seed(args.seed)
+    tf.random.set_seed(args.seed)
+    tf.config.threading.set_inter_op_parallelism_threads(args.threads)
+    tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+    if args.recodex:
+        tf.keras.utils.get_custom_objects()["glorot_uniform"] = tf.initializers.GlorotUniform(seed=args.seed)
+        tf.keras.utils.get_custom_objects()["orthogonal"] = tf.initializers.Orthogonal(seed=args.seed)
+        tf.keras.utils.get_custom_objects()["uniform"] = tf.initializers.RandomUniform(seed=args.seed)
+
+    # Report only errors by default
+    if not args.verbose:
+        os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+    # Create logdir name
+    args.logdir = os.path.join("logs", "{}-{}-{}".format(
+        os.path.basename(globals().get("__file__", "notebook")),
+        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+    ))
+
+    # Load the data
+    mnist = MNIST()
+
+    # Create the network and train
+    network = Network(args)
+    network.train(mnist, args)
+
+    # Compute test set accuracy and print it
+    accuracy = network.test(mnist, args)
+    with open("mnist_cnn.out", "w") as out_file:
+        print("{:.2f}".format(100 * accuracy), file=out_file)
---
+++ tasks/mnist_cnn.md
@@ -0,0 +1,75 @@
+### Assignment: mnist_cnn
+#### Date: Deadline: Apr 05, 23:59
+#### Points: 5 points
+#### Examples: mnist_cnn_example
+
+To pass this assignment, you will learn to construct basic convolutional
+neural network layers. Start with the
+[mnist_cnn.py](https://github.com/ufal/npfl114/tree/master/labs/04/mnist_cnn.py)
+template and assume the requested architecture is described by the `cnn`
+argument, which contains comma-separated specifications of the following layers:
+- `C-filters-kernel_size-stride-padding`: Add a convolutional layer with ReLU
+  activation and specified number of filters, kernel size, stride and padding.
+  Example: `C-10-3-1-same`
+- `CB-filters-kernel_size-stride-padding`: Same as
+  `C-filters-kernel_size-stride-padding`, but use batch normalization.
+  In detail, start with a convolutional layer **without bias and activation**,
+  then add batch normalization layer, and finally ReLU activation.
+  Example: `CB-10-3-1-same`
+- `M-kernel_size-stride`: Add max pooling with specified size and stride.
+  Example: `M-3-2`
+- `R-[layers]`: Add a residual connection. The `layers` contain a specification
+  of at least one convolutional layer (but not a recursive residual connection `R`).
+  The input to the specified layers is then added to their output.
+  Example: `R-[C-16-3-1-same,C-16-3-1-same]`
+- `F`: Flatten inputs. Must appear exactly once in the architecture.
+- `H-hidden_layer_size`: Add a dense layer with ReLU activation and specified
+  size. Example: `H-100`
+- `D-dropout_rate`: Apply dropout with the given dropout rate. Example: `D-0.5`
+
+An example architecture might be `--cnn=CB-16-5-2-same,M-3-2,F,H-100,D-0.5`.
+You can assume the resulting network is valid; it is fine to crash if it is not.
+
+After a successful ReCodEx submission, you can try obtaining the best accuracy
+on MNIST and then advance to `cifar_competition`.
+
+#### Examples Start: mnist_cnn_example
+_Note that the results might be slightly different, depending on whether you use
+GPU or on your CPU type._
+
+- `python3 mnist_cnn.py --batch_size=50 --cnn=F --epochs=3 --seed=42 --threads=1`
+```
+Epoch 1/3 - train_accuracy: 0.8625 - val_accuracy: 0.9288
+Epoch 2/3 - train_accuracy: 0.9094 - val_accuracy: 0.9330
+Epoch 3/3 - train_accuracy: 0.9164 - val_accuracy: 0.9386
+Test accuracy: 0.9210
+```
+- `python3 mnist_cnn.py --batch_size=50 --cnn=F,H-100 --epochs=3 --seed=42 --threads=1`
+```
+Epoch 1/3 - train_accuracy: 0.9109 - val_accuracy: 0.9612
+Epoch 2/3 - train_accuracy: 0.9583 - val_accuracy: 0.9704
+Epoch 3/3 - train_accuracy: 0.9698 - val_accuracy: 0.9730
+Test accuracy: 0.9698
+```
+- `python3 mnist_cnn.py --batch_size=50 --cnn=F,H-200,D-0.5 --epochs=3 --seed=42 --threads=1`
+```
+Epoch 1/3 - train_accuracy: 0.8899 - val_accuracy: 0.9658
+Epoch 2/3 - train_accuracy: 0.9426 - val_accuracy: 0.9716
+Epoch 3/3 - train_accuracy: 0.9518 - val_accuracy: 0.9758
+Test accuracy: 0.9716
+```
+- `python3 mnist_cnn.py --batch_size=50 --cnn=C-16-3-2-same,M-3-2,F,H-100 --epochs=3 --seed=42 --threads=1`
+```
+Epoch 1/3 - train_accuracy: 0.9104 - val_accuracy: 0.9702
+Epoch 2/3 - train_accuracy: 0.9674 - val_accuracy: 0.9754
+Epoch 3/3 - train_accuracy: 0.9749 - val_accuracy: 0.9814
+Test accuracy: 0.9772
+```
+- `python3 mnist_cnn.py --batch_size=50 --cnn=CB-16-3-2-same,M-3-2,F,H-100 --epochs=3 --seed=42 --threads=1`
+```
+Epoch 1/3 - train_accuracy: 0.9179 - val_accuracy: 0.9744
+Epoch 2/3 - train_accuracy: 0.9704 - val_accuracy: 0.9792
+Epoch 3/3 - train_accuracy: 0.9776 - val_accuracy: 0.9804
+Test accuracy: 0.9770
+```
+#### Examples End:
