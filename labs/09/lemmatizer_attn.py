5155199f5df2129d10bc52ceffce5d7720538806
Milan Straka <milan@strakovi.com>
2020-04-29 23:53:36 +0200

    Fix error in ReCodEx execution of the lemmatizer templates.

--- labs/09/lemmatizer_attn.py
+++ labs/09/lemmatizer_attn.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+import argparse
+import datetime
+import os
+import re
+
 import numpy as np
 import tensorflow as tf
 import tensorflow_addons as tfa
@@ -222,12 +227,8 @@ class Network:
 
         return metrics
 
-if __name__ == "__main__":
-    import argparse
-    import datetime
-    import os
-    import re
 
+if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", default=10, type=int, help="Batch size.")
@@ -236,22 +237,28 @@ if __name__ == "__main__":
     parser.add_argument("--max_sentences", default=5000, type=int, help="Maximum number of sentences to load.")
     parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
     parser.add_argument("--rnn_dim", default=64, type=int, help="RNN cell dimension.")
+    parser.add_argument("--seed", default=42, type=int, help="Random seed.")
     parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-    args = parser.parse_args()
+    parser.add_argument("--verbose", default=False, action="store_true", help="Verbose TF logging.")
+    args = parser.parse_args([] if "__file__" not in globals() else None)
 
-    # Fix random seeds and number of threads
-    np.random.seed(42)
-    tf.random.set_seed(42)
-    if args.recodex:
-        tf.keras.utils.get_custom_objects()["glorot_uniform"] = lambda: tf.initializers.glorot_uniform(seed=42)
-        tf.keras.utils.get_custom_objects()["orthogonal"] = lambda: tf.initializers.orthogonal(seed=42)
-        tf.keras.utils.get_custom_objects()["uniform"] = lambda: tf.initializers.RandomUniform(seed=42)
+    # Fix random seeds and threads
+    np.random.seed(args.seed)
+    tf.random.set_seed(args.seed)
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+    if args.recodex:
+        tf.keras.utils.get_custom_objects()["glorot_uniform"] = tf.initializers.GlorotUniform(seed=args.seed)
+        tf.keras.utils.get_custom_objects()["orthogonal"] = tf.initializers.Orthogonal(seed=args.seed)
+        tf.keras.utils.get_custom_objects()["uniform"] = tf.initializers.RandomUniform(seed=args.seed)
+
+    # Report only errors by default
+    if not args.verbose:
+        os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
     # Create logdir name
     args.logdir = os.path.join("logs", "{}-{}-{}".format(
-        os.path.basename(__file__),
+        os.path.basename(globals().get("__file__", "notebook")),
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
         ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
     ))
