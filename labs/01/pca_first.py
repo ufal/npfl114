Correct covariance matrix computation.

labs/01/pca_first.py
@@ -42,7 +42,7 @@ def main(args):
     mean = None
 
     # TODO: Compute the covariance matrix. The covariance matrix is
-    #   (data - mean)^T * (data - mean)
+    #   (data - mean)^T * (data - mean) / data.shape[0]
     # where transpose can be computed using `tf.transpose` and matrix
     # multiplication using either Python operator @ or `tf.linalg.matmul`.
     cov = None