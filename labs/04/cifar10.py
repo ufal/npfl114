81420b28246fe889f3bb0e6f2990adb004ce345e
Milan Straka <milan@strakovi.com>
2021-03-23 13:36:57 +0100

    Update cifar10 to support evaluation.

--- labs/04/cifar10.py
+++ labs/04/cifar10.py
@@ -14,6 +14,7 @@ class CIFAR10:
         def __init__(self, data, shuffle_batches, seed=42):
             self._data = data
             self._data["images"] = self._data["images"].astype(np.float32) / 255
+            self._data["labels"] = self._data["labels"].ravel()
             self._size = len(self._data["images"])
 
             self._shuffler = np.random.RandomState(seed) if shuffle_batches else None
@@ -48,3 +49,32 @@ class CIFAR10:
         for dataset in ["train", "dev", "test"]:
             data = dict((key[len(dataset) + 1:], cifar[key][:size.get(dataset, None)]) for key in cifar if key.startswith(dataset))
             setattr(self, dataset, self.Dataset(data, shuffle_batches=dataset == "train"))
+
+    @staticmethod
+    def evaluate(gold_dataset, predictions):
+        gold = gold_dataset.data["labels"]
+
+        if len(predictions) != len(gold):
+            raise RuntimeError("The predictions are of different size than gold data: {} vs {}".format(
+                len(predictions), len(gold)))
+
+        correct = sum(gold[i] == predictions[i] for i in range(len(gold)))
+        return 100 * correct / len(gold)
+
+    @staticmethod
+    def evaluate_file(gold_dataset, predictions_file):
+        predictions = [int(line) for line in predictions_file]
+        return CIFAR10.evaluate(gold_dataset, predictions)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--evaluate", default=None, type=str, help="Prediction file to evaluate")
+    parser.add_argument("--dataset", default="dev", type=str, help="Gold dataset to evaluate")
+    args = parser.parse_args()
+
+    if args.evaluate:
+        with open(args.evaluate, "r", encoding="utf-8-sig") as predictions_file:
+            accuracy = CIFAR10.evaluate_file(getattr(CIFAR10(), args.dataset), predictions_file)
+        print("CIFAR10 accuracy: {:.2f}%".format(accuracy))
