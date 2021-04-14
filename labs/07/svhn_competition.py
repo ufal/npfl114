adb5b49b863e17cc8f7a5db8edf0f9a753537c03
Milan Straka <milan@strakovi.com>
2021-04-14 14:38:40 +0200

    Add svhn_competition template.

---
+++ labs/07/svhn_competition.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import argparse
+import datetime
+import os
+import re
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2") # Report only TF errors by default
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+import bboxes_utils
+import efficient_net
+from svhn_dataset import SVHN
+
+# TODO: Define reasonable defaults and optionally more parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
+parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
+parser.add_argument("--seed", default=42, type=int, help="Random seed.")
+parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+
+def main(args):
+    # Fix random seeds and threads
+    np.random.seed(args.seed)
+    tf.random.set_seed(args.seed)
+    tf.config.threading.set_inter_op_parallelism_threads(args.threads)
+    tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+
+    # Create logdir name
+    args.logdir = os.path.join("logs", "{}-{}-{}".format(
+        os.path.basename(globals().get("__file__", "notebook")),
+        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+    ))
+
+    # Load the data
+    svhn = SVHN()
+
+    # Load the EfficientNet-B0 model
+    efficientnet_b0 = efficient_net.pretrained_efficientnet_b0(include_top=False)
+
+    # TODO: Create the model and train it
+    model = ...
+
+    # Generate test set annotations, but in args.logdir to allow parallel execution.
+    os.makedirs(args.logdir, exist_ok=True)
+    with open(os.path.join(args.logdir, "svhn_competition.txt"), "w", encoding="utf-8") as predictions_file:
+        # TODO: Predict the digits and their bounding boxes on the test set.
+        # Assume that for a single test image we get
+        # - `predicted_classes`: a 1D array with the predicted digits,
+        # - `predicted_bboxes`: a [len(predicted_classes), 4] array with bboxes;
+        for predicted_classes, predicted_bboxes in ...:
+            output = []
+            for label, bbox in zip(predicted_classes, predicted_bboxes):
+                output += [label] + bbox
+            print(*output, file=predictions_file)
+
+if __name__ == "__main__":
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+    main(args)
