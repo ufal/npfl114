b12d67b1829136b94a44ec7e54431b96392a1e9b
Milan Straka <milan@strakovi.com>
2022-02-28 16:48:20 +0100

    Add mnist_regularization assignment.

---
+++ labs/03/mnist_regularization.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+import argparse
+import datetime
+import os
+import re
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2") # Report only TF errors by default
+
+import numpy as np
+import tensorflow as tf
+
+from mnist import MNIST
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
+parser.add_argument("--dropout", default=0, type=float, help="Dropout regularization.")
+parser.add_argument("--epochs", default=30, type=int, help="Number of epochs.")
+parser.add_argument("--hidden_layers", default=[400], nargs="*", type=int, help="Hidden layer sizes.")
+parser.add_argument("--l2", default=0, type=float, help="L2 regularization.")
+parser.add_argument("--label_smoothing", default=0, type=float, help="Label smoothing.")
+parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
+parser.add_argument("--seed", default=42, type=int, help="Random seed.")
+parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+# If you add more arguments, ReCodEx will keep them with your default values.
+
+def main(args: argparse.Namespace) -> float:
+    # Fix random seeds and threads
+    np.random.seed(args.seed)
+    tf.random.set_seed(args.seed)
+    tf.config.threading.set_inter_op_parallelism_threads(args.threads)
+    tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+
+    # Create logdir name
+    args.logdir = os.path.join("logs", "{}-{}-{}".format(
+        os.path.basename(globals().get("__file__", "notebook")),
+        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+    ))
+
+    # Load data
+    mnist = MNIST(size={"train": 5000})
+
+    # TODO: Create the model and incorporate the L2 regularization and dropout:
+    # - L2 regularization:
+    #   If `args.l2` is nonzero, create a `tf.keras.regularizers.L2` regularizer
+    #   and use it for all kernels (but not biases) of all Dense layers.
+    # - Dropout:
+    #   Add a `tf.keras.layers.Dropout` with `args.dropout` rate after the Flatten
+    #   layer and after each Dense hidden layer (but not after the output Dense layer).
+
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=[MNIST.H, MNIST.W, MNIST.C]))
+    for hidden_layer in args.hidden_layers:
+        model.add(tf.keras.layers.Dense(hidden_layer, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(MNIST.LABELS, activation=tf.nn.softmax))
+
+    # TODO: Implement label smoothing.
+    # Apply the given smoothing. You will need to change the
+    # `SparseCategorical{Crossentropy,Accuracy}` to `Categorical{Crossentropy,Accuracy}`
+    # because `label_smoothing` is supported only by `CategoricalCrossentropy`.
+    # That means you also need to modify the labels of all three datasets
+    # (i.e., `mnist.{train,dev,test}.data["labels"]`) from indices of the gold class
+    # to a full categorical distribution (you can use either NumPy or there is
+    # a helper method also in the `tf.keras.utils`).
+
+    model.compile(
+        optimizer=tf.optimizers.Adam(),
+        loss=tf.losses.SparseCategoricalCrossentropy(),
+        metrics=[tf.metrics.SparseCategoricalAccuracy(name="accuracy")],
+    )
+
+    tb_callback = tf.keras.callbacks.TensorBoard(args.logdir, histogram_freq=1, update_freq=100, profile_batch=0)
+
+    def evaluate_test(epoch, logs):
+        if epoch + 1 == args.epochs:
+            test_logs = model.evaluate(
+                mnist.test.data["images"], mnist.test.data["labels"], batch_size=args.batch_size, return_dict=True, verbose=0,
+            )
+            logs.update({"val_test_" + name: value for name, value in test_logs.items()})
+
+    logs = model.fit(
+        mnist.train.data["images"], mnist.train.data["labels"],
+        batch_size=args.batch_size, epochs=args.epochs,
+        validation_data=(mnist.dev.data["images"], mnist.dev.data["labels"]),
+        callbacks=[tf.keras.callbacks.LambdaCallback(on_epoch_end=evaluate_test), tb_callback],
+    )
+
+    # Return test accuracy for ReCodEx to validate
+    return logs.history["val_test_accuracy"][-1]
+
+if __name__ == "__main__":
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+    main(args)
---
+++ tasks/mnist_regularization.md
@@ -0,0 +1,56 @@
+### Assignment: mnist_regularization
+#### Date: Deadline: Mar 14, 7:59 a.m.
+#### Points: 3 points
+#### Tests: mnist_regularization_tests
+
+You will learn how to implement three regularization methods in this assignment.
+Start with the
+[mnist_regularization.py](https://github.com/ufal/npfl114/tree/master/labs/03/mnist_regularization.py)
+template and implement the following:
+- Allow using dropout with rate `args.dropout`. Add a dropout layer after the
+  first `Flatten` and also after all `Dense` hidden layers (but not after the
+  output layer).
+- Allow using L2 regularization with weight `args.l2`. Use
+  `tf.keras.regularizers.L1L2` as a regularizer for all kernels (but not
+  biases) of all `Dense` layers (including the last one).
+- Allow using label smoothing with weight `args.label_smoothing`. Instead
+  of `SparseCategoricalCrossentropy`, you will need to use
+  `CategoricalCrossentropy` which offers `label_smoothing` argument.
+
+In ReCodEx, there will be six tests tests (two for each regularization methods) and
+you will get half a point for passing each one.
+
+In addition to submitting the task in ReCodEx, also run the following
+variations and observe the results in TensorBoard (notably training, development
+and test set accuracy and loss):
+- dropout rate `0`, `0.3`, `0.5`, `0.6`, `0.8`;
+- l2 regularization `0`, `0.001`, `0.0001`, `0.00001`;
+- label smoothing `0`, `0.1`, `0.3`, `0.5`.
+
+#### Tests Start: mnist_regularization_tests
+_Note that your results may be slightly different, depending on your CPU type and whether you use a GPU._
+- `python3 mnist_regularization.py --epochs=1 --dropout=0.3`
+```
+loss: 0.7987 - accuracy: 0.7616 - val_loss: 0.3175 - val_accuracy: 0.9110 - val_test_loss: 0.3825 - val_test_accuracy: 0.8882
+```
+- `python3 mnist_regularization.py --epochs=1 --dropout=0.5`
+```
+loss: 1.0807 - accuracy: 0.6528 - val_loss: 0.3719 - val_accuracy: 0.8972 - val_test_loss: 0.4476 - val_test_accuracy: 0.8743
+```
+- `python3 mnist_regularization.py --epochs=1 --l2=0.001`
+```
+loss: 0.9748 - accuracy: 0.8374 - val_loss: 0.5730 - val_accuracy: 0.9188 - val_test_loss: 0.6294 - val_test_accuracy: 0.9049
+```
+- `python3 mnist_regularization.py --epochs=1 --l2=0.0001`
+```
+loss: 0.6501 - accuracy: 0.8396 - val_loss: 0.3136 - val_accuracy: 0.9210 - val_test_loss: 0.3704 - val_test_accuracy: 0.9075
+```
+- `python3 mnist_regularization.py --epochs=1 --label_smoothing=0.1`
+```
+loss: 0.9918 - accuracy: 0.8436 - val_loss: 0.7645 - val_accuracy: 0.9254 - val_test_loss: 0.8047 - val_test_accuracy: 0.9095
+```
+- `python3 mnist_regularization.py --epochs=1 --label_smoothing=0.3`
+```
+loss: 1.5068 - accuracy: 0.8428 - val_loss: 1.3686 - val_accuracy: 0.9332 - val_test_loss: 1.3936 - val_test_accuracy: 0.9125
+```
+#### Tests End:
