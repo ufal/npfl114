9f3864254be77a0a40b5d474ab2d467e1b2b748a
Milan Straka <milan@strakovi.com>
2023-03-10 08:14:30 +0100

    Clarify what would happen if the dataset size were odd.

--- labs/04/mnist_multiple.py
+++ labs/04/mnist_multiple.py
@@ -95,7 +95,8 @@ class Model(tf.keras.Model):
 
         # TODO: If `training`, shuffle the data with `buffer_size=10_000` and `seed=args.seed`.
 
-        # TODO: Combine pairs of examples by creating batches of size exactly 2.
+        # TODO: Combine pairs of examples by creating batches of size exactly 2 (you would throw
+        # away the last example if the original dataset size were odd; but in MNIST it is even).
 
         # TODO: Map pairs of images to elements suitable for our model. Notably,
         # the elements should be pairs `(input, output)`, with
