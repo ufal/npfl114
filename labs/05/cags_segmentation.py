2b938bd8d4013eecd23fe2d8d40bb5f0378088a1
Milan Straka <milan@strakovi.com>
2023-03-27 10:58:46 +0200

    Add the debug parameter to cags_segmentation template.

--- labs/05/cags_segmentation.py
+++ labs/05/cags_segmentation.py
@@ -11,19 +11,23 @@ import tensorflow as tf
 from cags_dataset import CAGS
 
 # TODO: Define reasonable defaults and optionally more parameters.
-# Also, you can set the number of the threads 0 to use all your CPU cores.
+# Also, you can set the number of threads to 0 to use all your CPU cores.
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", default=..., type=int, help="Batch size.")
+parser.add_argument("--debug", default=False, action="store_true", help="If given, run functions eagerly.")
 parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 
 
 def main(args: argparse.Namespace) -> None:
-    # Fix random seeds and threads
+    # Set the random seed and the number of threads.
     tf.keras.utils.set_random_seed(args.seed)
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+    if args.debug:
+        tf.config.run_functions_eagerly(True)
+        tf.data.experimental.enable_debug_mode()
 
     # Create logdir name
     args.logdir = os.path.join("logs", "{}-{}-{}".format(
